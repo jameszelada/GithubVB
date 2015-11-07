c8107e69ea1108d33cfec0c24a25f07687642063
jameszelada <jameszelada@gmail.com>
2015-11-07 16:22:52 -0600

    Revision #0002
    A new button was added to the form

--- VBWF/VBWF/Form1.vb
+++ VBWF/VBWF/Form1.vb
@@ -4,4 +4,9 @@
         Dim variable As String = "Jaime"
         MessageBox.Show("I'm just trying this shit "+variable)
     End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Dim variable As String = "Jaime"
+        MessageBox.Show("Another Change " + variable)
+    End Sub
 End Class
