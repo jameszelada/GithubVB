60e2b88f827b3ffa06bab0833c9704266b836b8c
jameszelada <jameszelada@gmail.com>
2015-11-07 16:16:32 -0600

    Revision #0001
    First Update of the project

---
+++ VBWF/VBWF/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim variable As String = "Jaime"
+        MessageBox.Show("I'm just trying this shit "+variable)
+    End Sub
+End Class
